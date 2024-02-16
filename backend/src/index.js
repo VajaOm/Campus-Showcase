ff8718153150493b41cef3d6789c08e4155427a3
Om Vaja <vajaom09@gmail.com>
2024-02-16 18:51:19 +0530

    mongodb database connection establish

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import connectionDB from './db/index.js';
+import { app } from './app.js';
+import dotenv from 'dotenv';
+
+dotenv.config({path: './env'});
+
+connectionDB()
+.then(()=> {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server is running at port ${process.env.PORT || 5000}`)
+    })
+})
+.catch((err) => {
+    console.log("Mongodb connection failed : ",err);
+})
