e0a87b5f0cd2bcc06790b6d591a9c43f38ceb8ce
Om Vaja <vajaom09@gmail.com>
2024-02-16 18:55:55 +0530

    early cors error handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+const configurationCors = {
+    origin : process.env.CORS_ORIGIN,
+    methods : 'GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS',
+    credentials: true
+};
+
+app.use(cors(configurationCors));
 
 export {app};
